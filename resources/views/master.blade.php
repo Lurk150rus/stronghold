48315d82bff8aecf5fb58b3b776de19c8cc1c62d
k.sasyk <unknownsomebody@yandex.ru>
2021-11-07 13:06:08 +0300

    fixes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,7 +23,7 @@
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="{{route('index')}}">Все товары</a></li>
-                <li ><a href="{{route('categories')}}">Категории</a>
+                <li ><a href="{{route('categories')}}/">Категории</a>
                 </li>
                 <li ><a href="{{route('basket')}}">В корзину</a></li>
                 <li><a href="">Сбросить проект в начальное состояние</a></li>
