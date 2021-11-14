569e50fadbb9ae3feca732181cdf317f28d5c28a
k.sasyk <unknownsomebody@yandex.ru>
2021-11-14 21:35:40 +0300

    fix blade

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,15 +1,18 @@
 @extends('master', ["file"=>'product'])
 @section('title', 'Товар' . $product->code)
 @section('content')
+
     <h1>{{ $product->name }}</h1>
     <h2>{{$product->category->name}}</h2>
     <p>Цена: <b>{{$product->price}} ₽</b></p>
-    <img src="{{ Storage::url($product->image) }}" alt="{{$product->name}}">
+    <div>
+        <img src="{{ Storage::url($product->image) }}" alt="{{$product->name}}">
+    </div>
+    <br/>
     <p>{{$product->description}}</p>
 
     <form action="{{route('basketAdd', $product->id)}}" method="POST">
         <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
-
         @csrf
     </form>
 @endsection
