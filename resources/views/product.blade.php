142f102b8a88352a476692f2a82a9916d7773bfa
Kirill Sasyk <unknownsomebody@yandex.ru>
2022-01-03 22:53:00 +0300

    breadcrumbs for product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,6 +2,14 @@
 @section('title', 'Товар' . $product->code)
 @section('content')
 
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('index') }}">Главная</a></li>
+            <li class="breadcrumb-item"><a href="{{route('category', [$product->category->code])}}">{{$product->category->name}}</a></li>
+            <li class="breadcrumb-item active" aria-current="page">{{$product->name}}</li>
+        </ol>
+    </nav>
+
     <h1>{{ $product->name }}</h1>
     <h2>{{$product->category->name}}</h2>
     <p>Цена: <b>{{$product->price}} ₽</b></p>
