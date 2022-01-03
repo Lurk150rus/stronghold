9ec9ceb9287f8058a59fea8e766788c207a86d3c
Kirill Sasyk <unknownsomebody@yandex.ru>
2022-01-03 22:58:12 +0300

    fix height for footer

--- public/js/equlize.js
+++ public/js/equlize.js
@@ -9,7 +9,7 @@ $(window).on("load resize", function () {
     let wrapHeight = $("body").innerHeight();
     let footerHeight = $("#footer").innerHeight();
     if (wrapHeight - footerHeight <= 900){
-        let footerHeight += 200;
+        let footerHeight = footerHeight + 200;
         $("#footer")[0].classList.add('navbar-fixed-bottom');
         $("#footer").css("bottom", "-"+ footerHeight + "px");
     } else{
