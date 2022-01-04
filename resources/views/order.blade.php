d88e2ea48f5f64d3c2e51033f02f3e375c100477
Kirill Sasyk <unknownsomebody@yandex.ru>
2022-01-04 16:04:39 +0300

    fix form

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -7,13 +7,13 @@
         <p>Общая стоимость: <b>{{$order->calculateFullSum()}} ₽.</b></p>
         <form action="{{route('basketConfirm')}}" method="POST">
             <div>
-                <p>Укажите свои имя и номер телефона, чтобы наш менеджер мог с вами связаться:</p>
+                <p>Укажите своё имя и номер телефона, чтобы наш менеджер мог с вами связаться:</p>
 
                 <div class="container">
                     <div class="form-group">
                         <label for="name" class="control-label col-lg-offset-3 col-lg-2">Имя: </label>
                         <div class="col-lg-4">
-                            <input type="text" name="name" id="name" value="" class="form-control">
+                            <input required type="text" name="name" id="name" value="" class="form-control">
                         </div>
                     </div>
                     <br>
@@ -21,17 +21,10 @@
                     <div class="form-group">
                         <label for="phone" class="control-label col-lg-offset-3 col-lg-2">Номер телефона: </label>
                         <div class="col-lg-4">
-                            <input type="text" name="phone" id="phone" value="" class="form-control">
+                            <input required type="text" name="phone" id="phone" value="" class="form-control">
                         </div>
                     </div>
-                    <br>
-                    <br>
-                    <div class="form-group">
-                        <label for="name" class="control-label col-lg-offset-3 col-lg-2">Email: </label>
-                        <div class="col-lg-4">
-                            <input type="text" name="email" id="email" value="" class="form-control">
-                        </div>
-                    </div>
+
                 </div>
                 <br>
                 @csrf
